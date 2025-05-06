edb6da29287891c98248ff3a838415b6c636d966
Leaflet <leaflet@leafok.com>
2025-05-06 13:30:26 +0800

    Update copyright and license information

--- include/io.h
+++ include/io.h
@@ -1,16 +1,15 @@
 /***************************************************************************
 							io.h  -  description
 							 -------------------
-	begin                : Mon Oct 18 2004
-	copyright            : (C) 2004 by Leaflet
-	email                : leaflet@leafok.com
+	Copyright            : (C) 2004-2025 by Leaflet
+	Email                : leaflet@leafok.com
  ***************************************************************************/
 
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
--- include/screen.h
+++ include/screen.h
@@ -1,8 +1,7 @@
 /***************************************************************************
                             screen.h  -  description
                              -------------------
-    begin                : Mon Oct 18 2004
-    copyright            : (C) 2004 by Leaflet
+    copyright            : (C) 2004-2025 by Leaflet
     email                : leaflet@leafok.com
  ***************************************************************************/
 
@@ -10,7 +9,7 @@
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
