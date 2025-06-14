ba72037b6c348144547396bc21957d47a4796cfa
Leaflet <leaflet@leafok.com>
2025-06-14 10:00:16 +0800

    Update text

--- src/section_list_display.c
+++ src/section_list_display.c
@@ -140,7 +140,8 @@ static int section_list_draw_screen(const char *sname, const char *stitle, const
 	show_top(str_section_master, stitle, str_section_name);
 	moveto(2, 0);
 	prints("返回[\033[1;32m←\033[0;37m,\033[1;32mESC\033[0;37m] 选择[\033[1;32m↑\033[0;37m,\033[1;32m↓\033[0;37m] "
-		   "阅读[\033[1;32m→\033[0;37m,\033[1;32mENTER\033[0;37m] 发表文章[\033[1;32mCtrl-P\033[0;37m] %s[\033[1;32mn\033[0;37m]\033[m",
+		   "阅读[\033[1;32m→\033[0;37m,\033[1;32mENTER\033[0;37m] 发表[\033[1;32mCtrl-P\033[0;37m] "
+		   "修改[\033[1;32mCtrl-E\033[0;37m] %s[\033[1;32mn\033[0;37m]\033[m",
 		   (display_nickname ? "显示用户名" : "显示昵称"));
 	moveto(3, 0);
 	if (display_nickname)
@@ -283,16 +284,16 @@ static int display_article_key_handler(int *p_key, DISPLAY_CTX *p_ctx)
 		if (section_topic_view_mode)
 		{
 			snprintf(p_ctx->msg, sizeof(p_ctx->msg),
-					 "| 返回[\033[32m←\033[33m,\033[32mESC\033[33m] | "
-					 "同主题阅读[\033[32m↑\033[33m/\033[32m↓\033[33m] | "
-					 "模式[\033[32mp\033[33m] | 帮助[\033[32mh\033[33m] |");
+					 "| 返回[\033[32m←\033[33m,\033[32mESC\033[33m] "
+					 "同主题阅读[\033[32m↑\033[33m/\033[32m↓\033[33m] "
+					 "切换[\033[32mp\033[33m] 回复[\033[32mr\033[33m] 帮助[\033[32mh\033[33m] |");
 		}
 		else
 		{
 			snprintf(p_ctx->msg, sizeof(p_ctx->msg),
-					 "| 返回[\033[32m←\033[33m,\033[32mESC\033[33m] | "
-					 "移动[\033[32m↑\033[33m/\033[32m↓\033[33m/\033[32mPgUp\033[33m/\033[32mPgDn\033[33m] | "
-					 "模式[\033[32mp\033[33m] | 帮助[\033[32mh\033[33m] |");
+					 "| 返回[\033[32m←\033[33m,\033[32mESC\033[33m] "
+					 "移动[\033[32m↑\033[33m/\033[32m↓\033[33m/\033[32mPgUp\033[33m/\033[32mPgDn\033[33m] "
+					 "切换[\033[32mp\033[33m] 回复[\033[32mr\033[33m] 帮助[\033[32mh\033[33m] |");
 		}
 		*p_key = 0;
 		break;
