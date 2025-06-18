f50181271088e2e203836fb24e1007a03d4de8f9
Leaflet <leaflet@leafok.com>
2025-06-18 12:03:35 +0800

    Add copyright info

--- src/test_ssh_server.c
+++ src/test_ssh_server.c
@@ -1,4 +1,20 @@
-// This test was written base on libssh example/proxy.c
+/***************************************************************************
+					test_ssh_server.c  -  description
+							 -------------------
+	Copyright            : (C) 2004-2025 by Leaflet
+	Email                : leaflet@leafok.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// This test was written based on libssh example/proxy.c
 
 #include "log.h"
 #include <stdio.h>
