22b661611d3b00af0965bc71037b4e7b21517be0
Leaflet <leaflet@leafok.com>
2025-06-20 16:58:15 +0800

    Adjust shm size and count

--- src/section_list.c
+++ src/section_list.c
@@ -42,8 +42,8 @@ union semun
 #define SECTION_TRY_LOCK_WAIT_TIME 1 // second
 #define SECTION_TRY_LOCK_TIMES 10
 
-#define ARTICLE_BLOCK_PER_SHM 10000		  // sizeof(ARTICLE_BLOCK) * ARTICLE_BLOCK_PER_SHM is the size of each shm segment to allocate
-#define ARTICLE_BLOCK_SHM_COUNT_LIMIT 8 // limited by length (8-bit) of proj_id in ftok(path, proj_id)
+#define ARTICLE_BLOCK_PER_SHM 1000		  // sizeof(ARTICLE_BLOCK) * ARTICLE_BLOCK_PER_SHM is the size of each shm segment to allocate
+#define ARTICLE_BLOCK_SHM_COUNT_LIMIT 80 // limited by length (8-bit) of proj_id in ftok(path, proj_id)
 #define ARTICLE_BLOCK_PER_POOL (ARTICLE_BLOCK_PER_SHM * ARTICLE_BLOCK_SHM_COUNT_LIMIT)
 
 #define CALCULATE_PAGE_THRESHOLD 100 // Adjust to tune performance of moving topic between sections
