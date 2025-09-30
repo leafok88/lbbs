b91614b14302ee5ee786f4e3e434917158ec40e8
Leaflet <leaflet@leafok.com>
2025-09-30 12:54:09 +0800

    Add display_article_meta() Fix bug of edit / delete article in empty section

---
+++ include/article_op.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+					   article_op.h  -  description
+							 -------------------
+	Copyright            : (C) 2004-2025 by Leaflet
+	Email                : leaflet@leafok.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _ARTICLE_OP_H_
+#define _ARTICLE_OP_H_
+
+#include <stdint.h>
+
+extern int display_article_meta(int32_t aid);
+
+#endif //_ARTICLE_OP_H_
