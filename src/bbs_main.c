747091d10b2af38169625905c74cbc5c258f0b09
Leaflet <leaflet@leafok.com>
2025-10-18 13:54:12 +0800

    Add handling of KEY_NULL exception

--- src/bbs_main.c
+++ src/bbs_main.c
@@ -283,14 +283,16 @@ int bbs_charset_select()
 	char msg[LINE_BUFFER_LEN];
 	int ch;
 
+	snprintf(msg, sizeof(msg),
+			 "\rChoose character set in 5 seconds [UTF-8, GBK]: [U/g]");
+
 	while (!SYS_server_exit)
 	{
-		snprintf(msg, sizeof(msg),
-				 "\rChoose character set in 5 seconds [UTF-8, GBK]: [U/g]");
-
 		ch = press_any_key_ex(msg, 5);
 		switch (ch)
 		{
+		case KEY_NULL:
+			return -1;
 		case KEY_TIMEOUT:
 		case CR:
 		case 'u':
