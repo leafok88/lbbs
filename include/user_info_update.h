43acad44ae213e2f94a16dfdb8a6a754c5aa06af
Leaflet <46334625+leafok88@users.noreply.github.com>
2025-11-11 18:03:16 +0800

    Delete bbs_userinfo_const_t enum from header
    Removed unused enum for user info constants.

--- include/user_info_update.h
+++ include/user_info_update.h
@@ -11,13 +11,6 @@
 #ifndef _USER_INFO_UPDATE_
 #define _USER_INFO_UPDATE_
 
-enum bbs_userinfo_const_t
-{
-    BBS_user_intro_line_len = 256,
-    BBS_user_sign_max_len = 4096,
-    BBS_user_sign_max_line = 10,
-};
-
 extern int user_intro_edit(int);
 extern int user_sign_edit(int);
 
